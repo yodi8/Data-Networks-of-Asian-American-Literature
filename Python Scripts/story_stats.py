7a3e88967c5a22aed4cab06341434fb06ce66647
ryan32241 <ronghao.si@gmail.com>
2025-05-22 19:17:59 -0400

    story_stats.py
    added Flesch Reading Score

--- Python Scripts/story_stats.py
+++ Python Scripts/story_stats.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
+import syllapy
+import string
 
 file_name = "Li-TheReasonWhy-2020.txt"
 path = "Short Stories As Text\\" + file_name
@@ -37,7 +39,21 @@ def words_per_sentece(text):
 
     result = sum(count_words_per_sentence)/len(count_words_per_sentence)
     return result
+    
 
+def flesch_reading_ease(text):
+    score = (206.835 - 1.015*(words_per_sentence(text))
+             - 84.6*(syllable_counter(text)/count_words(text)))
+    return score
+
+def syllable_counter(text):
+    words = text.split()
+    syllable_count = 0
+
+    for word in words:
+        word = word.strip(string.punctuation)
+        syllable_count += syllapy.count(word)
+    return syllable_count
 
 
 with open(path, "r", encoding="utf-8") as file:
@@ -45,4 +61,5 @@ with open(path, "r", encoding="utf-8") as file:
     print(f"Word Count: {count_words(text)}")
     print(f"Average word length: {average_word_length(text)}")
     print(f"Words Per Sentence: {words_per_sentece(text)}")
+    print(f"Flesch Reading Score: {flesch_reading_ease(text)}")
 
